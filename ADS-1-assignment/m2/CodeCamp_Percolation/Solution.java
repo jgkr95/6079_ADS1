2c214b474fed43e242ec52637c26113c0279c5a6
jgkr95 <jgkr95@msitprogram.net>
2018-09-25 18:40:06 +0530

    "check_style_score -> 6 of 24 passed. style score: 0/1 "

--- ADS-1-assignment/m2/CodeCamp_Percolation/Solution.java
+++ ADS-1-assignment/m2/CodeCamp_Percolation/Solution.java
@@ -8,56 +8,56 @@
 // }
 import java.util.Scanner;
 class Percolation {
-	int opensites,rowcount,bottomrowcount;
+	int opensites = 0,rowcount = 0,bottomrowcount = 0;
+	WeightedQuickUnionUF uf;
 	boolean[][] grid;
+	int size;
 	public Percolation(int n) {
 		grid = new boolean[n][n];
+		uf = new WeightedQuickUnionUF(n*n+2);
+		size = n*n;
 	}
 	public void open(int row, int col) {
-		grid[row - 1][col - 1] = true;
-		if(row-1==0) rowcount++;
-		if(row-1==grid.length-1) bottomrowcount++;
+		grid[row][col] = true;
+		if(row==0) rowcount++;
+		if(row==grid.length-1) bottomrowcount++;
 		opensites++;
+		if (col != grid.length - 1 && grid[row][col + 1]) {
+			uf.union(row*grid.length+col, row*grid.length+(col+1));
+		}
+		if (row != grid.length - 1 && grid[row + 1][col]) {
+			uf.union(row*grid.length+col, row+1*grid.length+col);
+
+		}
+		if (col != 0 && grid[row][col - 1]) {
+			uf.union(row*grid.length+col, row*grid.length+(col-1));
+
+		}
+		if (row != 0 && grid[row - 1][col]) {
+			uf.union((row-1)*grid.length+col, (row-1)*grid.length+col);
+		}
 	}
 	public boolean isOpen(int row, int col) {
-		return grid[row - 1][col - 1];
+		return grid[row][col];
+		
 	}
-	public boolean isFull(int row, int col) {
-		return (opensites == row - 1 * col - 1);
-	}
+	// public boolean isFull(int row, int col) {
+	// 	return (opensites == row - 1 * col - 1);
+	// }
 	public int numberOfOpenSites() {
 		return opensites;
 	}
 	public boolean percolates() {
-		int i = 0, j = 0;
-		if(rowcount==0||bottomrowcount==0) return false;
-		while (i < grid.length) {
-			while (j < grid.length) {
-				if (grid[i][j]) {
-					if (j != grid.length - 1 && grid[i][j + 1]) {
-
-					}
-					if (i != grid.length - 1 && grid[i + 1][j]) {
-
-					}
-					if (j != 0 && grid[i][j - 1]) {
-
-					}
-					if (i != 0 && grid[i - 1][j]) {
-
-					}
-				}
-			}
-		}
-		return true;
+		return uf.connected(size,size+1);
 	}
 }
+
 public final class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		Percolation p = new Percolation(scan.nextInt());
 		while (scan.hasNext()) {
-			p.open(scan.nextInt(), scan.nextInt());
+			p.open(scan.nextInt()-1, scan.nextInt()-1);
 		}
 		System.out.println(p.percolates());
 	}
