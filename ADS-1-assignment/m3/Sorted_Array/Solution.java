aec5a490846c5af5492b37ce3e2d91f35f9d8901
jgkr95 <jgkr95@msitprogram.net>
2018-09-26 18:36:20 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS-1-assignment/m3/Sorted_Array/Solution.java
+++ ADS-1-assignment/m3/Sorted_Array/Solution.java
@@ -32,11 +32,11 @@ public class Solution {
         int[] finalarray = new int[size];
         for(int i=0;i<size;i++) {
             System.out.println(m+" "+n);
-            if(arr1[m]<arr2[n]&&m<arr1.length) {
+            if(m<arr1.length&&arr1[m]<arr2[n]) {
                 finalarray[i] = arr1[m];
                 m = m+1;
             }
-            else if(arr1[m]>arr2[n]&&n<arr2.length) {
+            else if(n<arr2.length&&arr1[m]>arr2[n]) {
                 finalarray[i] = arr2[n];
                 n = n+1;
             }
