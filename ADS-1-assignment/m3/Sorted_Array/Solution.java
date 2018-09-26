5654c96105bf8d1ac9d9f92ca4bedb2bdfc18632
jgkr95 <jgkr95@msitprogram.net>
2018-09-26 18:58:36 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS-1-assignment/m3/Sorted_Array/Solution.java
+++ ADS-1-assignment/m3/Sorted_Array/Solution.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for solution.
+ */
 public class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
 
     }
+    /**
+     * { main method to read the input }.
+     *
+     * @param      args  The arguments of string type
+     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int sizeoffirstarray = scan.nextInt();
@@ -13,47 +24,47 @@ public class Solution {
         scan.nextLine();
         String[] data1 = scan.nextLine().split(",");
         String[] data2 = scan.nextLine().split(",");
-        // System.out.println(data1.length);
-        for(int i=0;i<sizeoffirstarray;i++) {
-            // System.out.println(data1[i]+"===="+"===");
+        for (int i = 0; i < sizeoffirstarray; i++) {
             firstarray[i] = Integer.parseInt(data1[i].trim());
         }
 
-        for(int i=0;i<data2.length;i++) {
-            // System.out.println(data2[i]);
+        for (int i = 0; i < data2.length; i++) {
             secondarray[i] = Integer.parseInt(data2[i].trim());
-        } 
-        sort(firstarray,secondarray);
+        }
+        sort(firstarray, secondarray);
     }
+    /**
+     * { It will add the sorted arrays in to single array sorted }
+     *
+     * @param      arr1  The arr1 is sorted array1
+     * @param      arr2  The arr2 is sorted array2
+     */
     public static void sort(int[] arr1, int[] arr2) {
-       int m=0,n=0,i=0;
-        // System.out.println(Arrays.toString(arr1));
-        // System.out.println(Arrays.toString(arr2));
+        int m = 0, n = 0, i = 0;
         int size =  arr1.length + arr2.length;
         int[] finalarray = new int[size];
-        while(m<arr1.length&&n<arr2.length) {
+        while (m < arr1.length && n < arr2.length) {
             // System.out.println(m+" "+n);
-            if(arr1[m]<arr2[n]) {
+            if (arr1[m] < arr2[n]) {
                 finalarray[i++] = arr1[m];
-                m = m+1;
-            }
-            else if(arr1[m]>arr2[n]) {
+                m = m + 1;
+            } else if (arr1[m] > arr2[n]) {
                 finalarray[i++] = arr2[n];
-                n = n+1;
+                n = n + 1;
             }
         }
-        if(arr1.length>arr2.length) {
-            while(i<size) {
+        if (arr1.length > arr2.length) {
+            while (i < size) {
                 finalarray[i++] = arr1[m++];
             }
-            
+
         }
-        if(arr2.length>arr1.length) {
-            while(i<size) {
+        if (arr2.length > arr1.length) {
+            while (i < size) {
                 finalarray[i++] = arr2[n++];
             }
-            
+
         }
-        System.out.println(Arrays.toString(finalarray).replace(" ","").replace("[","").replace("]",""));
-   }
+        System.out.println(Arrays.toString(finalarray).replace(" ", "").replace("[", "").replace("]", ""));
+    }
 }
