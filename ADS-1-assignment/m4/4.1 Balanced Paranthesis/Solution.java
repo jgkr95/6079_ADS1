fa8b9111882d085d972fec4f21831647a3ce8f6f
jgkr95 <jgkr95@msitprogram.net>
2018-09-27 16:08:09 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS-1-assignment/m4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public final class Solution{
+	private Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int nooflines = scan.nextInt();
+		for(int i=0;i<nooflines;i++) {
+			
+			String line = scan.next();
+			stack paren = new stack(line);
+			System.out.println(paren.check());
+		}
+	}
+	
+}
+class stack {
+	private String data;
+	char[] braces;
+	int size;
+
+	public stack(final String input) {
+		data =  input;
+		braces = new char[50];
+		size = 0; 
+	}
+	public String check() {
+		for(int i=0;i<data.length();i++) {
+			if(data.charAt(i)==('(')||data.charAt(i)==('{')||data.charAt(i)==('{')) {
+				push(data.charAt(i));
+			}
+			else if(data.charAt(i)==(')')) {
+				if(braces[size]==('(')) {
+					pop(data.charAt(i));
+				}
+			}else if(data.charAt(i)==('}')) {
+				if(braces[size]==('{')) {
+					pop(data.charAt(i));
+				}
+			}else if(data.charAt(i)==(']')) {
+				if(braces[size]==('[')) {
+					pop(data.charAt(i));
+				}
+			}
+		}
+		if(size==0) {
+			return "YES";
+		}
+		return "NO";
+	}
+	public void push(char in) {
+		braces[size++] = in;
+	}
+	public void pop(char in) {
+		if(size!=0) {
+			size--;
+		}
+		
+	}
+
+}
