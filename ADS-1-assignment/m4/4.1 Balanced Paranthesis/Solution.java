f1f0607b79df688312063b0796571fbe556e4e87
jgkr95 <jgkr95@msitprogram.net>
2018-09-27 16:19:14 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS-1-assignment/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1-assignment/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
-public final class Solution{
+public final class Solution {
 	private Solution() {
 
 	}
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		int nooflines = scan.nextInt();
-		for(int i=0;i<nooflines;i++) {
-			
+		for (int i = 0; i < nooflines; i++) {
+
 			String line = scan.next();
 			stack paren = new stack(line);
 			System.out.println(paren.check());
 		}
 	}
-	
+
 }
 class stack {
 	private String data;
@@ -24,30 +24,31 @@ class stack {
 	public stack(final String input) {
 		data =  input;
 		braces = new char[200];
-		size = 0; 
+		size = 0;
 	}
 	public String check() {
-		for(int i=0;i<data.length();i++) {
-			if(data.charAt(i)=='('||data.charAt(i)=='{'||data.charAt(i)=='{') {
+		for (int i = 0; i < data.length(); i++) {
+			if (data.charAt(i) == '(' || data.charAt(i) == '{' || data.charAt(i) == '{') {
 				push(data.charAt(i));
-			}if(size!=0) {
-			if(data.charAt(i)==')') {
-				if(braces[size-1]=='(') {
-					pop(data.charAt(i));
+			} if (size != 0) {
+				if (data.charAt(i) == ')') {
+					if (braces[size - 1] == '(') {
+						pop(data.charAt(i));
+					}
+				} else if (data.charAt(i) == '}') {
+					if (braces[size - 1] == '{') {
+						pop(data.charAt(i));
+					}
+				} else if (data.charAt(i) == ']') {
+					if (braces[size - 1] == '[') {
+						pop(data.charAt(i));
+					}
+				} else {
+					break;
 				}
-			}else if(data.charAt(i)=='}') {
-				if(braces[size-1]=='{') {
-					pop(data.charAt(i));
-				}
-			}else if(data.charAt(i)==']') {
-				if(braces[size-1]=='[') {
-					pop(data.charAt(i));
-				}
-			} else{
-				return "NO++++";	
-			}}
+			}
 		}
-		if(size==0) {
+		if (size == 0) {
 			return "YES";
 		}
 		return "NO";
@@ -56,10 +57,10 @@ class stack {
 		braces[size++] = in;
 	}
 	public void pop(char in) {
-		if(size!=0) {
+		if (size != 0) {
 			size--;
 		}
-		
+
 	}
 
 }
