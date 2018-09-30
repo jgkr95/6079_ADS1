f42b347b0fc7a98b341a144ab406b5d94601c97e
jgkr95 <jgkr95@msitprogram.net>
2018-09-30 23:47:00 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ ADS-1-assignment/m5/5.2 Josephus Problem/Solution.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+class Josephus {
+	Node head;
+	int size;
+	class Node {
+		Node next;
+		int data;
+		Node(int dataa) {
+			data = dataa;
+		}
+	}
+
+	public void insert(int data) {
+		Node newnode = new Node(data);
+		size++;
+		if (head == null) {
+			head = newnode;
+		} else {
+			Node current = head;
+			while (current.next != null) {
+				current = current.next;
+			}
+			current.next = newnode;
+		}
+	}
+	public void circlepos() {
+		Node temp = head;
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+		temp.next = head;
+	}
+	public void sword(int total, int killposition) {
+		Node temp1 = head, temp2 = head;
+		int count = 1;
+		while (temp1.next != temp1) {
+			while (count != killposition) {
+				temp2 = temp1;
+				temp1 = temp1.next;
+				count++;
+			}
+			System.out.print(temp1.data + " ");
+			temp2.next = temp1.next;
+			temp1 = temp2.next;
+		}
+	}
+}
+public final class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Josephus jobj = new Josephus();
+		int limit = scan.nextInt();
+		while (limit >= 0) {
+			int total = scan.nextInt();
+			int kill = scan.nextInt();
+			for (int i = 0; i < total; i++) {
+				jobj.insert(i);
+			}
+			jobj.circlepos();
+			jobj.sword(total, kill);
+		}
+
+	}
+}
