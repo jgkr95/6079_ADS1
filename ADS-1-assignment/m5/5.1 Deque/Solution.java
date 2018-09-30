db0531bf4f3aea9c847ff61c7ba51f8e0f9ed737
jgkr95 <jgkr95@msitprogram.net>
2018-09-30 20:33:32 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS-1-assignment/m5/5.1 Deque/Solution.java
+++ ADS-1-assignment/m5/5.1 Deque/Solution.java
@@ -1,24 +1,56 @@
 import java.util.Scanner;
+/**
+ * Class for deque.
+ *
+ * @param      <Item>  The item is of Generic type
+ */
 class Deque<Item> {
     Node head;
-    Node tail;
     int size;
+    /**
+     * Constructs the object.
+     */
     Deque() {
+        size = 0;
+        head = null;
 
     }
+    /**
+     * Class for node.
+     */
     class Node {
         Node next;
         Item data;
+        /**
+         * Constructs the object.
+         *
+         * @param      dataa  The dataa is of Item type
+         */
         Node(Item dataa) {
             data = dataa;
         }
     }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
     public boolean isEmpty() {
         return size == 0;
     }
+    /**
+     * { Size of the deque }.
+     *
+     * @return     { size of the deque }
+     */
     public int size() {
         return size;
     }
+    /**
+     * Pushes a left.
+     *
+     * @param      item  The item
+     */
     public void pushLeft(Item item) {
         Node newnode = new Node(item);
         newnode.next = head;
@@ -27,6 +59,11 @@ class Deque<Item> {
         print();
 
     }
+    /**
+     * Pushes a right.
+     *
+     * @param      item  The item of Item type
+     */
     public void pushRight(Item item) {
         Node newnode = new Node(item);
         size++;
@@ -43,6 +80,9 @@ class Deque<Item> {
         }
         print();
     }
+    /**
+     * { deletes element from left side }.
+     */
     public void popLeft() {
         if (size == 0) {
             System.out.println("Deck is empty");
@@ -52,6 +92,9 @@ class Deque<Item> {
             print();
         }
     }
+    /**
+     * { Deletes a element from right side }.
+     */
     public void popRight() {
         if (size == 0) {
             System.out.println("Deck is empty");
@@ -65,6 +108,9 @@ class Deque<Item> {
             print();
         }
     }
+    /**
+     * { Prints all the elements of deque }.
+     */
     public void print() {
         Node cur = head;
         // String[] content = new String[size];
@@ -83,7 +129,20 @@ class Deque<Item> {
         }
     }
 }
+/**
+ * Class for solution.
+ */
 class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * { This is main method to handle input }.
+     *
+     * @param      args  The arguments are of String array type
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Deque d = new Deque();
