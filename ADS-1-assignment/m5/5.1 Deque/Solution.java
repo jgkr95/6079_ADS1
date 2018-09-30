66679570a493d9a8c163c621b18c60652e5d20a1
jgkr95 <jgkr95@msitprogram.net>
2018-09-30 19:33:25 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ ADS-1-assignment/m5/5.1 Deque/Solution.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+class Deque<Item> {
+	Node head;
+	Node tail;
+	int size;
+	Deque() {
+
+	}
+	class Node {
+		Node next;
+		Item data;
+		Node(Item dataa) {
+			data = dataa;
+		}
+	}
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	public int size() {
+		return size;
+	}
+	public void pushLeft(Item item) {
+		Node newnode = new Node(item);
+		newnode.next = head;
+		head = newnode;
+		size++;
+
+	}
+	public void pushRight(Item item) {
+		Node newnode = new Node(item);
+		size++;
+		newnode.next = null;
+
+		if (head == null) {
+			head = newnode;
+		} else {
+			Node cur = head;
+			while (cur.next != null) {
+				cur = cur.next;
+			}
+			cur.next = newnode;
+		}
+	}
+	public void popLeft() {
+		head = head.next;
+	}
+	public void popRight() {
+		Node cur = head;
+		while (cur.next != null) {
+			cur = cur.next;
+		}
+		cur.next = null;
+
+	}
+}
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Deque d = new Deque();
+		int n = Integer.parseInt(sc.nextLine());
+		while (sc.hasNext()) {
+			String[] str = sc.nextLine().split(" ");
+			switch(str[0]) {
+				case "isEmpty":
+				System.out.println(d.isEmpty());
+				break;
+				case "size":
+				System.out.println(d.size());
+				break;
+				case "pushLeft":
+				d.pushLeft(Integer.parseInt(str[1]));
+				break;
+				case "pushRight":
+				d.pushRight(str[1]);
+				break;
+				case "popLeft":
+				d.popLeft();
+				break;
+				case "popRight":
+				d.popRight();
+				break;
+				default:
+				break;
+			}
+		}
+		
+	}
+}
