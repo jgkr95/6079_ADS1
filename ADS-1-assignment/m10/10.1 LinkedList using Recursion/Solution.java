f2810c967f05d9dfff6edb9103185310eef48024
jgkr95 <jgkr95@msitprogram.net>
2018-10-04 20:20:16 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS-1-assignment/m10/10.1 LinkedList using Recursion/Solution.java
+++ ADS-1-assignment/m10/10.1 LinkedList using Recursion/Solution.java
@@ -1,122 +1,122 @@
 import java.util.Scanner;
 class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		LinkedList list = new LinkedList();
-		while (scan.hasNext()) {
-			String[] tokens = scan.nextLine().split(" ");
-			try {
-				switch (tokens[0]) {
-				case "insertAt":
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        LinkedList list = new LinkedList();
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(" ");
+            try {
+                switch (tokens[0]) {
+                case "insertAt":
 
-					list.insertAt(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
-					list.display();
+                    list.insertAt(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+                    list.display();
 
-					break;
+                    break;
 
-				case "reverse":
+                case "reverse":
 
-					list.reverse();
+                    list.reverse();
 
-					list.display();
+                    list.display();
 
-					break;
-				default :
-					break;
+                    break;
+                default :
+                    break;
 
-				}
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
 
-		}
-	}
+        }
+    }
 }
 class LinkedList {
 
-	Node head;
-	Node tail;
-	int size;
+    Node head;
+    Node tail;
+    int size;
 
-	class Node {
-		int data;
-		Node next;
+    class Node {
+        int data;
+        Node next;
 
-		public Node(int data) {
-			this.data = data;
-			this.next = null;
-		}
-	}
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
-	public LinkedList() {
-		this.head = null;
-		this.tail = null;
-		this.size = 0;
-	}
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
 
-	public void insertAt(int index, int data) throws Exception {
+    public void insertAt(int index, int data) throws Exception {
 
-		if (index < 0 || index > size) {
-			throw new Exception("Can't insert at this position.");
-		}
+        if (index < 0 || index > size) {
+            throw new Exception("Can't insert at this position.");
+        }
 
-		head = insertAt(head, new Node(data), index, 0);
-		size++;
+        head = insertAt(head, new Node(data), index, 0);
+        size++;
 
-	}
+    }
 
-	private Node insertAt(Node curr, Node newNode, int index, int currIndex) {
-		if (curr == null) {
-			// newNode.next = head;
-			// System.out.println("yep");
-			return newNode;
-		} else if (currIndex == index) {
-			newNode.next = curr;
-			return newNode;
-		}
+    private Node insertAt(Node curr, Node newNode, int index, int currIndex) {
+        if (curr == null) {
+            // newNode.next = head;
+            // System.out.println("yep");
+            return newNode;
+        } else if (currIndex == index) {
+            newNode.next = curr;
+            return newNode;
+        }
 
-		curr.next = insertAt(curr.next, newNode, index, currIndex + 1);
-		return curr;
+        curr.next = insertAt(curr.next, newNode, index, currIndex + 1);
+        return curr;
 
-	}
-
-
-	public void reverse() throws Exception {
-		if (head == null) {
-			throw new Exception("No elements to reverse.");
-		}
-		reverse(head, null);
-	}
-
-	private Node reverse(Node curr, Node prev) {
+    }
 
 
+    public void reverse() throws Exception {
+        if (head == null) {
+            throw new Exception("No elements to reverse.");
+        }
+        reverse(head, null);
+    }
 
-		if (curr.next == null) {
-			head = curr;
-			head.next = prev;
-			return null;
-		}
+    private Node reverse(Node curr, Node prev) {
 
-		Node temp = curr.next;
-		curr.next = prev;
 
-		reverse(temp, curr);
-		return head;
-	}
 
-	public void display() {
-		Node curr = head;
-		String result = "";
-		while (curr != null) {
-			if (curr.next == null) {
-				result += curr.data;
-			} else {
-				result += curr.data + ", ";
-			}
-			curr = curr.next;
-		}
+        if (curr.next == null) {
+            head = curr;
+            head.next = prev;
+            return null;
+        }
 
-		System.out.println(result);
-	}
+        Node temp = curr.next;
+        curr.next = prev;
+
+        reverse(temp, curr);
+        return head;
+    }
+
+    public void display() {
+        Node curr = head;
+        String result = "";
+        while (curr != null) {
+            if (curr.next == null) {
+                result += curr.data;
+            } else {
+                result += curr.data + ", ";
+            }
+            curr = curr.next;
+        }
+
+        System.out.println(result);
+    }
 }
