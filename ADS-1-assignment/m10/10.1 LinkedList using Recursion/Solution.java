86ddfd274a1601dbd86f597ed380117213de018f
jgkr95 <jgkr95@msitprogram.net>
2018-10-04 20:19:26 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- ADS-1-assignment/m10/10.1 LinkedList using Recursion/Solution.java
+++ ADS-1-assignment/m10/10.1 LinkedList using Recursion/Solution.java
@@ -1,62 +1,62 @@
 import java.util.Scanner;
-class Solution{
+class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		LinkedList list = new LinkedList();
-		while(scan.hasNext()) {
+		while (scan.hasNext()) {
 			String[] tokens = scan.nextLine().split(" ");
-			switch(tokens[0]) {
+			try {
+				switch (tokens[0]) {
 				case "insertAt":
-				try {
-								list.insertAt(Integer.parseInt(tokens[1]),Integer.parseInt(tokens[2]));
-								list.display();
-							}
-							catch(Exception e) {
-								System.out.println(e.getMessage());
-								break;
-							}
+
+					list.insertAt(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+					list.display();
+
+					break;
+
 				case "reverse":
-							try {
-								list.reverse();
 
-								// list.display();
-							} catch(Exception e) {
-								System.out.println(e.getMessage());
-							}
+					list.reverse();
+
+					// list.display();
+
 					break;
-				default : 
+				default :
 					break;
 
+				}
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
 			}
 
 		}
 	}
 }
-class LinkedList{
+class LinkedList {
 
 	Node head;
 	Node tail;
 	int size;
-	
-	class Node{
+
+	class Node {
 		int data;
 		Node next;
 
-		public Node(int data){
+		public Node(int data) {
 			this.data = data;
 			this.next = null;
 		}
 	}
 
-	public LinkedList(){
+	public LinkedList() {
 		this.head = null;
 		this.tail = null;
 		this.size = 0;
 	}
 
-	public void insertAt(int index, int data) throws Exception{
+	public void insertAt(int index, int data) throws Exception {
 
-		if(index < 0 || index > size){
+		if (index < 0 || index > size) {
 			throw new Exception("Can't insert at this position.");
 		}
 
@@ -65,35 +65,34 @@ class LinkedList{
 
 	}
 
-	private Node insertAt(Node curr, Node newNode, int index, int currIndex){
-		if(curr == null){
+	private Node insertAt(Node curr, Node newNode, int index, int currIndex) {
+		if (curr == null) {
 			// newNode.next = head;
 			// System.out.println("yep");
 			return newNode;
-		}
-		else if(currIndex == index){
+		} else if (currIndex == index) {
 			newNode.next = curr;
 			return newNode;
 		}
 
-		curr.next = insertAt(curr.next, newNode, index, currIndex+1);
+		curr.next = insertAt(curr.next, newNode, index, currIndex + 1);
 		return curr;
 
 	}
 
 
 	public void reverse() throws Exception {
-		if(head == null){
+		if (head == null) {
 			throw new Exception("No elements to reverse.");
 		}
 		reverse(head, null);
 	}
 
-	private Node reverse(Node curr, Node prev){
+	private Node reverse(Node curr, Node prev) {
 
 
 
-		if(curr.next == null){
+		if (curr.next == null) {
 			head = curr;
 			head.next = prev;
 			return null;
@@ -102,19 +101,18 @@ class LinkedList{
 		Node temp = curr.next;
 		curr.next = prev;
 
-		reverse(temp,curr);
+		reverse(temp, curr);
 		return head;
 	}
 
-	public void display(){
+	public void display() {
 		Node curr = head;
 		String result = "";
-		while(curr!=null){
-			if(curr.next == null){
+		while (curr != null) {
+			if (curr.next == null) {
 				result += curr.data;
-			}
-			else{
-				result += curr.data+", ";
+			} else {
+				result += curr.data + ", ";
 			}
 			curr = curr.next;
 		}
