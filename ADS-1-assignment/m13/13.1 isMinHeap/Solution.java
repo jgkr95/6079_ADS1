3351efd3b0ce1242e58b890bab369242de0cf77d
jgkr95 <jgkr95@msitprogram.net>
2018-10-08 19:16:05 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- ADS-1-assignment/m13/13.1 isMinHeap/Solution.java
+++ ADS-1-assignment/m13/13.1 isMinHeap/Solution.java
@@ -45,11 +45,11 @@ class MinHeap<K extends Comparable<K>> {
             if (array[last].compareTo(array[last / 2]) >= 0) {
                 last--;
             } else {
-                System.out.println("false");
+                System.out.println("true");
                 return;
             }
         }
-        System.out.println("true");
+        System.out.println("false");
     }
 }
 // class Minheap {
