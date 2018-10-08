e33f7fe279720ed1998d366c2a6dde24b60b722d
jgkr95 <jgkr95@msitprogram.net>
2018-10-08 19:12:56 +0530

    "check_style_score -> 3 of 5 passed. style score: 0/1 "

--- ADS-1-assignment/m13/13.1 isMinHeap/Solution.java
+++ ADS-1-assignment/m13/13.1 isMinHeap/Solution.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class Solution {
+class Solution<K extends Comparable<K>> {
 	Solution() {
 
 	}
@@ -18,36 +18,62 @@ class Solution {
 			for(int j = 0;j<tokens.length;j++) {
 				heap[j+1]=tokens[j];
 			}
-			Minheap m = new Minheap(heap);
+			MinHeap m = new MinHeap();
+			m.isMinHeap(heap);
 			// System.out.println(Arrays.toString(heap));
-			System.out.println(m.check());
+			// System.out.println(m.check());
 
 		}
 
 	}
 }
-class Minheap {
-	int size;
-	Comparable[] heapArray;
-	Minheap(Comparable[] arr) {
-		heapArray = arr;
-		size = arr.length;
-	}
-	public boolean check() {
-		if(heapArray.length==2) {
-			return true;
-		}
-		return isMinHeap(1);
-	}
-	private boolean isMinHeap(int k) {
-		if (k > size) return true;
-		int left = 2 * k;
-		int right = 2 * k + 1;
-		if (left  <= size && greater(k,left))  return false;
-		if (right <= size && greater(k,right)) return false;
-		return isMinHeap(left) && isMinHeap(right);
-	}
-	private boolean greater(int i, int j) {
-		return heapArray[i].compareTo(heapArray[j]) < 0;
-	}
+class MinHeap<K extends Comparable<K>> {
+    /**
+     * an empty constructor.
+     */
+    MinHeap() {
+    }
+    /**
+     * checks whether it is min heap or not.
+     *the time complexiety is N.
+     *N is the size of the array.
+     * @param  array  which is given as input.
+     */
+    public void isMinHeap(final K[] array) {
+        int last = array.length - 1;
+        while (last > 1) {
+            if (array[last].compareTo(array[last / 2]) >= 0) {
+                last--;
+            } else {
+                System.out.println("false");
+                return;
+            }
+        }
+        System.out.println("true");
+    }
 }
+// class Minheap {
+// 	int size;
+// 	Comparable[] heapArray;
+// 	Minheap(Comparable[] arr) {
+// 		heapArray = arr;
+// 		size = arr.length;
+// 	}
+// 	public boolean check() {
+// 		if(heapArray.length==2) {
+// 			return true;
+// 		}
+// 		return isMinHeap(1);
+// 	}
+// 	private boolean isMinHeap(int k) {
+// 		if (k > size) return true;
+// 		int left = 2 * k;
+// 		int right = 2 * k + 1;
+// 		if (left  <= size && greater(k,left))  return false;
+// 		if (right <= size && greater(k,right)) return false;
+// 		return isMinHeap(left) && isMinHeap(right);
+// 	}
+// 	private boolean greater(int i, int j) {
+// 		return heapArray[i].compareTo(heapArray[j]) < 0;
+// 	}
+// }
