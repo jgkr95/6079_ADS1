8ec644a0a7fd4f2c7a5a2f65b7e553d3a415723d
jgkr95 <jgkr95@msitprogram.net>
2018-10-13 11:45:28 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-assignment/m18/Week-3_Assignment-1/Solution.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Solution {
+	public static void main(String[] args) {
+		// BinarySearchSymbolTable<String, Integer> bst =
+		//           new BinarySearchSymbolTable<String, Integer>();
+
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		AddStocks as = new AddStocks(n * 6);
+		scan.nextLine();
+		// bst = new BinarySearchSymbolTable<String, Integer>(n);
+		for (int i = 0; i < n * 6; i++) {
+			String[] tokens = scan.nextLine().split(",");
+			as.addStock(new Stocks(tokens[0], Double.parseDouble(tokens[1])));
+			as.sort(n);
+		}
+	}
+}
+class AddStocks {
+	Stocks[] stack;
+	int size;
+	Sorting s = new Sorting();
+	AddStocks(int n) {
+		stack = new Stocks[20];
+		size = 0;
+	}
+	public void addStock(Stocks s) {
+		stack[size++] = s;
+	}
+	public void sort(int n) {
+		for (int i = 0; i < 6; i++) {
+			s.sort(stack, i*n, i*n+n);
+			for(int j=i*n;j<i*n+5;j++) {
+				System.out.println(stack[j].getName()+" "+stack[j].getChange());
+			}
+			System.out.println();
+			for(int j=i*n-5;j<i*n;j++) {
+				System.out.println(stack[j].getName()+" "+stack[j].getChange());
+			}
+		}
+	}
+	class Sorting {
+		public void sort(Stocks[] st, int lo, int hi) {
+			for (int i = lo; i < hi; i++) {
+				for (int j = i + 1; j < hi; j++) {
+					if (less(st[i], st[j])) {
+						swap(st[i], st[j]);
+					}
+				}
+			}
+		}
+		public void swap(Stocks s1, Stocks s2) {
+			Stocks temp = s1;
+			s2 = s1;
+			s1 = temp;
+		}
+		public boolean less(Stocks s1, Stocks s2) {
+			if (s1.getChange()-s2.getChange() < 0) {
+				return true;
+			}
+			if (s1.getChange()-s2.getChange() == 0) {
+				if (s1.getName().compareTo(s2.getName()) > 0) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+	}
+}
+
+class Stocks {
+	String name;
+	double change;
+	Stocks(String name1, double change1) {
+		name = name1;
+		change = change1;
+	}
+	public double getChange() {
+		return change;
+	}
+	public String getName() {
+		return name;
+	}
+}
