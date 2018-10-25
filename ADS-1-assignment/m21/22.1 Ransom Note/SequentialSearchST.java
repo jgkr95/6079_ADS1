6bec2201c0fd7755c21d5498a7b41243394d9c62
jgkr95 <jgkr95@msitprogram.net>
2018-10-25 22:00:23 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignment/m21/22.1 Ransom Note/SequentialSearchST.java
+++ ADS-1-assignment/m21/22.1 Ransom Note/SequentialSearchST.java
@@ -1,54 +1,52 @@
 /**
- *  The {@code SequentialSearchST} class represents an (unordered)
- *  symbol table of generic key-value pairs.
- *  It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
- *  <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
- *  It also provides a <em>keys</em> method for iterating over all of the keys.
- *  A symbol table implements the <em>associative array</em> abstraction:
- *  when associating a value with a key that is already in the symbol table,
- *  the convention is to replace the old value with the new value.
- *  The class also uses the convention that values cannot be {@code null}. Setting the
- *  value associated with a key to {@code null} is equivalent to deleting the key
- *  from the symbol table.
- *  <p>
- *  This implementation uses a singly-linked list and sequential search.
- *  It relies on the {@code equals()} method to test whether two keys
- *  are equal. It does not call either the {@code compareTo()} or
- *  {@code hashCode()} method. 
- *  The <em>put</em> and <em>delete</em> operations take linear time; the
- *  <em>get</em> and <em>contains</em> operations takes linear time in the worst case.
- *  The <em>size</em>, and <em>is-empty</em> operations take constant time.
- *  Construction takes constant time.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/31elementary">Section 3.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for sequential search st.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @param      <Key>    The key
+ * @param      <Value>  The value
  */
 public class SequentialSearchST<Key, Value> {
-    private int n;           // number of key-value pairs
+    /**.
+     * { var_description }
+     */
+    private int n;
+    /**.
+     * { item_description }
+     */// number of key-value pairs
     private Node first;      // the linked list of key-value pairs
-
-    // a helper linked list data type
+    /**
+     * Class for node.
+     */
     private class Node {
+        /**
+         * { var_description }.
+         */
         private Key key;
+        /**
+         * { var_description }.
+         */
         private Value val;
+        /**
+         * { var_description }.
+         */
         private Node next;
-
+        /**
+         * Constructs the object.
+         *
+         * @param      key   The key
+         * @param      val   The value
+         * @param      next  The next
+         */
         public Node(Key key, Value val, Node next)  {
             this.key  = key;
             this.val  = val;
             this.next = next;
         }
     }
-
     /**
      * Initializes an empty symbol table.
      */
     public SequentialSearchST() {
     }
-
     /**
      * Returns the number of key-value pairs in this symbol table.
      *
@@ -57,7 +55,6 @@ public class SequentialSearchST<Key, Value> {
     public int size() {
         return n;
     }
-
     /**
      * Returns true if this symbol table is empty.
      *
@@ -67,7 +64,6 @@ public class SequentialSearchST<Key, Value> {
     public boolean isEmpty() {
         return size() == 0;
     }
-
     /**
      * Returns true if this symbol table contains the specified key.
      *
@@ -77,7 +73,8 @@ public class SequentialSearchST<Key, Value> {
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public boolean contains(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        if (key == null) throw
+         new IllegalArgumentException("argument to contains() is null");
         return get(key) != null;
     }
 
@@ -85,12 +82,14 @@ public class SequentialSearchST<Key, Value> {
      * Returns the value associated with the given key in this symbol table.
      *
      * @param  key the key
-     * @return the value associated with the given key if the key is in the symbol table
+     * @return the value associated with the given key
+     *  if the key is in the symbol table
      *     and {@code null} if the key is not in the symbol table
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public Value get(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to get() is null"); 
+        if (key == null) throw 
+            new IllegalArgumentException("argument to get() is null");
         for (Node x = first; x != null; x = x.next) {
             if (key.equals(x.key))
                 return x.val;
@@ -98,10 +97,13 @@ public class SequentialSearchST<Key, Value> {
         return null;
     }
 
-    /**
-     * Inserts the specified key-value pair into the symbol table, overwriting the old 
-     * value with the new value if the symbol table already contains the specified key.
-     * Deletes the specified key (and its associated value) from this symbol table
+    /**.
+     * Inserts the specified key-value pair into the symbol table,
+     *  overwriting the old
+     * value with the new value if the symbol table already
+     *  contains the specified key.
+     * Deletes the specified key (and its associated value)
+     *  from this symbol table
      * if the specified value is {@code null}.
      *
      * @param  key the key
@@ -109,7 +111,8 @@ public class SequentialSearchST<Key, Value> {
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public void put(Key key, Value val) {
-        if (key == null) throw new IllegalArgumentException("first argument to put() is null"); 
+        if (key == null) throw
+         new IllegalArgumentException("first argument to put() is null");
         if (val == null) {
             delete(key);
             return;
@@ -125,20 +128,23 @@ public class SequentialSearchST<Key, Value> {
         n++;
     }
 
-    /**
-     * Removes the specified key and its associated value from this symbol table     
-     * (if the key is in this symbol table).    
+    /**.
+     * Removes specified key and its associated value from this symbol table
+     * (if the key is in this symbol table).
      *
      * @param  key the key
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public void delete(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to delete() is null"); 
+        if (key == null) throw
+         new IllegalArgumentException("argument to delete() is null");
         first = delete(first, key);
     }
-
-    // delete key in linked list beginning at Node x
-    // warning: function call stack too large if table is large
+    /**
+     * { item_description }.
+     * // delete key in linked list beginning at Node x
+     * // warning: function call stack too large if table is large
+     */
     private Node delete(Node x, Key key) {
         if (x == null) return null;
         if (key.equals(x.key)) {
@@ -149,20 +155,4 @@ public class SequentialSearchST<Key, Value> {
         return x;
     }
 
-
-    /**
-     * Returns all keys in the symbol table as an {@code Iterable}.
-     * To iterate over all of the keys in the symbol table named {@code st},
-     * use the foreach notation: {@code for (Key key : st.keys())}.
-     *
-     * @return all keys in the symbol table
-     */
-    // public Iterable<Key> keys()  {
-    //     Queue<Key> queue = new Queue<Key>();
-    //     for (Node x = first; x != null; x = x.next)
-    //         queue.enqueue(x.key);
-    //     return queue;
-    // }
-
-    }
-    
+}
