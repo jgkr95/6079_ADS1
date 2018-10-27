94b2df473f03e93a7ad5305d01799f8dcb7cefd5
jgkr95 <jgkr95@msitprogram.net>
2018-10-27 10:53:37 +0530

    "m24_a1_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-assignment/m24/1/Solution.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int size = Integer.parseInt(scan.nextLine());
+		AddStudent as = new AddStudent(size);
+		for (int i = 0; i < size; i++) {
+			String[] tokens = scan.nextLine().split(",");
+			as.add(new Student(Integer.parseInt((tokens[0])), tokens[1], Double.parseDouble(tokens[2])));
+		}
+		int queries = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < queries; i++) {
+			String[] tokens = scan.nextLine().split(" ");
+			Student sObj = as.get(Integer.parseInt(tokens[1]));
+			switch (tokens[2]) {
+			case "1":
+				System.out.println(sObj.getName());
+				break;
+			case "2":
+				System.out.println(sObj.getTotal());
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
+class Student {
+	int rollNumber;
+	String name;
+	double totalMarks;
+	Student(int rollNumber1, String name1, double totalMarks1) {
+		rollNumber = rollNumber1;
+		name = name1;
+		totalMarks = totalMarks1;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getTotal() {
+		return totalMarks;
+	}
+	public int getRollNumber() {
+		return rollNumber;
+	}
+}
+class AddStudent {
+	Student[] sInfo;
+	int size;
+	AddStudent(int size1) {
+		sInfo = new Student[size1];
+	}
+	public void add(Student s) {
+		sInfo[size++] = s;
+	}
+	public Student get(int rollNumber) {
+		for (int i = 0; i < size; i++) {
+			if (sInfo[i].getRollNumber()==rollNumber) {
+				return sInfo[i];
+			}
+		}
+		return null;
+	}
+
+}
