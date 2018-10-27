f19751017bee3b2c2a5cb78d4ca9cdf1e0373046
jgkr95 <jgkr95@msitprogram.net>
2018-10-27 11:05:27 +0530

    "m24_a1_1: 2/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignment/m24/1/Solution.java
+++ ADS-1-assignment/m24/1/Solution.java
@@ -12,12 +12,19 @@ class Solution {
 		for (int i = 0; i < queries; i++) {
 			String[] tokens = scan.nextLine().split(" ");
 			Student sObj = as.get(Integer.parseInt(tokens[1]));
+			if (sObj == null) {
+				System.out.println("Student doesn't exists...");
+			}
 			switch (tokens[2]) {
 			case "1":
 				System.out.println(sObj.getName());
 				break;
 			case "2":
-				System.out.println(sObj.getTotal());
+				if (sObj.getTotal() < 1) {
+					System.out.println("Student doesn't exists...");
+				} else {
+					System.out.println(sObj.getTotal());
+				}
 				break;
 			default:
 				break;
@@ -30,7 +37,7 @@ class Student {
 	String name;
 	double totalMarks;
 	Student() {
-		
+
 	}
 	Student(int rollNumber1, String name1, double totalMarks1) {
 		rollNumber = rollNumber1;
@@ -57,13 +64,13 @@ class AddStudent {
 		sInfo[size++] = s;
 	}
 	public Student get(int rollNumber) {
-		Student s=new Student();
+		Student s = new Student();
 		for (int i = 0; i < size; i++) {
-			if (sInfo[i].getRollNumber()==rollNumber) {
-				if(s==null) {
-					s=sInfo[i];
-				} else if(sInfo[i].getTotal()>s.getTotal()) {
-					s=sInfo[i];
+			if (sInfo[i].getRollNumber() == rollNumber) {
+				if (s == null) {
+					s = sInfo[i];
+				} else if (sInfo[i].getTotal() > s.getTotal()) {
+					s = sInfo[i];
 				}
 			}
 		}
